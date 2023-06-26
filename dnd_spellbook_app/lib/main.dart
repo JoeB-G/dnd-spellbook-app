6e925078c5e9f09c8ccc419b09171c060df0c0dc
JoeB-G <joeboddenglennon@hotmail.co.uk>
2023-06-26 12:53:04 +0100

    add supabase initial

--- dnd_spellbook_app/lib/main.dart
+++ dnd_spellbook_app/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:dnd_spellbook_app/screens/spellbook_level_0.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://jfgwsdleqfsayhywwhus.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmZ3dzZGxlcWZzYXloeXd3aHVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc3NzYzMDAsImV4cCI6MjAwMzM1MjMwMH0.t5j6b5vsf4qzM6kkBvE6i7EF43Q85fTnXw9TXMmBa0E',
+  );
   runApp(const MainApp());
 }
 
